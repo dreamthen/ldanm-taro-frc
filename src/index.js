bffa7b9340858181df00ccfbf10fc6379dd58daa
dreamthen <dreamthen.00@gmail.com>
2020-06-17 21:56:58 +0800

    build:升级版本

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import UserInfo from './UserInfo';
 import NavBar from './NavBar';
 import TabBar from './TabBar';
 import ImagePicker from './ImagePicker';
+import OutLogin from "./OutLogin";
 
 module.exports = {
     Login,
     UserInfo,
     NavBar,
     TabBar,
-    ImagePicker
+    ImagePicker,
+    OutLogin
 };
